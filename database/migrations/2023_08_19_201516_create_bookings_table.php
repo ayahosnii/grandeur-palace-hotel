ac0589c86f8a73d4324b471cd3b14d3df20b2217
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-08-20 20:12:24 +0300

    add

---
+++ database/migrations/2023_08_19_201516_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('room_id');
+            $table->dateTime('check_in');
+            $table->dateTime('check_out');
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
