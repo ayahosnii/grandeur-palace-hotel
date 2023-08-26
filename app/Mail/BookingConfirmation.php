fcddb31ca07029d7e87c375ecf3e8015b01e3cda
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-08-26 07:03:44 +0300

    add

--- app/Mail/BookingConfirmation.php
+++ app/Mail/BookingConfirmation.php
@@ -2,29 +2,32 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
+    use Illuminate\Bus\Queueable;
+    use Illuminate\Mail\Mailable;
+    use Illuminate\Queue\SerializesModels;
 
 class BookingConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
+
     public $bookingCode;
+
     /**
      * Create a new message instance.
      *
+     * @param  string  $bookingCode
      * @return void
      */
-
-
     public function __construct($bookingCode)
     {
         $this->bookingCode = $bookingCode;
     }
 
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
     public function build()
     {
         return $this->subject('Booking Confirmation')->view('emails.booking-confirmation');
